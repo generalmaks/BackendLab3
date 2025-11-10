b554c612956f3f7957af1e3ea435744f16d62f4a
generalmaks <schrinan@gmail.com>
2025-11-10 21:53:09 +0200

    Added EF Core to project

---
+++ BackendLab3/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackendLab3.Context;
+
+public partial class AppDbContext : DbContext
+{
+    private IConfiguration _config;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration config)
+        : base(options)
+    {
+        _config = config;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        => optionsBuilder.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- BackendLab3/Program.cs
+++ BackendLab3/Program.cs
@@ -1,3 +1,6 @@
+using BackendLab3.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,9 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
